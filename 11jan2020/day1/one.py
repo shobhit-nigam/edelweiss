aad0f361de55bebfc86eca498847cb32b12f17ac
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2020-01-12 14:17:03 +0530

    Add files via upload

---
+++ 11jan2020/day1/one.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+varx = 30
+
+
+# In[3]:
+
+
+print(varx)
+
+
+# In[4]:
+
+
+vary = 23.45
+
+
+# In[5]:
+
+
+print(vary)
+
+
+# In[6]:
+
+
+varz = "spanlabs"
+
+
+# In[7]:
+
+
+print(varz)
+
+
+# In[8]:
+
+
+print("hi")
+print("hello")
+
+
+# In[9]:
+
+
+import time
+
+
+# In[10]:
+
+
+print("hi")
+time.sleep(4)
+print("hello")
+
+
+# In[11]:
+
+
+help(time.sleep)
+
+
+# In[12]:
+
+
+type(varz)
+
+
+# In[13]:
+
+
+type(varx)
+
+
+# In[14]:
+
+
+vara = True 
+
+
+# In[15]:
+
+
+type(vara)
+
+
+# In[16]:
+
+
+print(Vara)
+
+
+# In[17]:
+
+
+print(vara)
+
+
+# In[18]:
+
+
+type(varx) == type(vary) 
+
+
+# In[19]:
+
+
+varx == vary
+
+
+# In[20]:
+
+
+varx = 30
+vary = 40
+varz = 50
+
+
+# In[21]:
+
+
+vara = varx + vary
+
+
+# In[22]:
+
+
+print(vara)
+
+
+# In[23]:
+
+
+varb = varz/varx
+
+
+# In[24]:
+
+
+print(varb)
+
+
+# In[25]:
+
+
+varx = 20
+vary = 40
+varz = 50
+
+
+# In[26]:
+
+
+vara = varz%varx
+
+
+# In[27]:
+
+
+print(vara)
+
+
+# In[28]:
+
+
+varb = varz//varx
+
+
+# In[29]:
+
+
+print(varb)
+
+
+# In[30]:
+
+
+varc = varz/varx
+
+
+# In[31]:
+
+
+print(varc)
+
+
+# In[32]:
+
+
+#    print("hello")
+
+
+# In[33]:
+
+
+#    comments 
+
+
+# In[34]:
+
+
+#    ipynb (iPython notebook)
+
+
+# In[ ]:
+
+
+
+
